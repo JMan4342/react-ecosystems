5b57e97fcea8a30817207501e0e9343061a6c522
Joseph Manning <jgmanning3@yahoo.com>
2022-05-06 15:24:50 -0700

    Create NewTodoForm component, add styling to components

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./TodoListItem.css";
 
 const TodoListItem = ({ todo }) => (
   <div className="todo-item-container">
@@ -10,4 +11,4 @@ const TodoListItem = ({ todo }) => (
   </div>
 );
 
-export default TodoListItem
+export default TodoListItem;
