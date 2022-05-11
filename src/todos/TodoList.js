ff1942963a1cd6ac2118fc96038e0b5e6f0bfa90
Joseph Manning <jgmanning3@yahoo.com>
2022-05-10 17:28:46 -0700

    Add Redux flow to Mark As Complete button

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,24 +2,29 @@ import React from "react";
 import { connect } from "react-redux";
 import TodoListItem from "./TodoListItem";
 import NewTodoForm from "./NewTodoForm";
-import {removeTodo} from "../actions";
+import { removeTodo, markTodoAsCompleted } from "../actions";
 import "./TodoList.css";
 
-const TodoList = ({ todos = [], onRemovePressed }) => (
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
   <div className="list-wrapper">
     <NewTodoForm />
     {todos.map((todo) => (
-      <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />
+      <TodoListItem
+        todo={todo}
+        onRemovePressed={onRemovePressed}
+        onCompletedPressed={onCompletedPressed}
+      />
     ))}
   </div>
 );
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   todos: state.todos,
 });
 
-const mapDispatchToProps = dispatch => ({
-  onRemovePressed: text => dispatch(removeTodo(text)),
-})
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+  onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
