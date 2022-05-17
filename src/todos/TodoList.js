1714bfadb68fc05f6a0c86adbbe9c39d7c68fd4a
Joseph Manning <jgmanning3@yahoo.com>
2022-05-17 14:26:00 -0700

    Install styled-components npm

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 import TodoListItem from "./TodoListItem";
 import NewTodoForm from "./NewTodoForm";
 import {
-  getTodos,
   getTodosLoading,
   getCompletedTodos,
   getIncompleteTodos,
